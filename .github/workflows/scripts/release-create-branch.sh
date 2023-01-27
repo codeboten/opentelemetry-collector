a77e49d4068845e4cd37f876805c54971b6a833c
Alex Boten <aboten@lightstep.com>
2023-01-27 10:30:53 -0800

    Update release-create-branch.sh

--- .github/workflows/scripts/release-create-branch.sh
+++ .github/workflows/scripts/release-create-branch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
-git config user.name opentelemetrybot
-git config user.email 107717825+opentelemetrybot@users.noreply.github.com
+git config user.name "$GITHUB_ACTOR"
+git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
 
 git checkout "${COMMIT}"
 BRANCH="release/v${CANDIDATE_STABLE}-v${CANDIDATE_BETA}"
