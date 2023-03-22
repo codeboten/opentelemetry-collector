73a3b86d1221546235a9ced20bbe4aa506c83b66
Alex Boten <aboten@lightstep.com>
2023-03-22 13:58:26 -0700

    Update renovate-config.js

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -3,7 +3,7 @@ module.exports = {
   branchPrefix: 'test-renovate/',
   username: 'renovate-release',
   gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
-  onboarding: false,
+  onboarding: true,
   platform: 'github',
   includeForks: true,
   repositories: [
