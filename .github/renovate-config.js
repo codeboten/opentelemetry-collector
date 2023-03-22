3dc1ec84846ead2aa06a1c9a2ce4daaa80dd8eae
Alex Boten <aboten@lightstep.com>
2023-03-22 12:41:56 -0700

    Update renovate-config.js

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  allowedPostUpgradeCommands: ['make gotidy'],
   branchPrefix: 'test-renovate/',
   username: 'renovate-release',
   gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
